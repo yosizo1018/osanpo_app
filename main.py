0710fe42faf632d55b0472d5328acab5f789bbad
yosizo1018 <uoly1018@gmail.com>
2024-02-14 15:47:47 +0900

    m4

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ st.title('ようこそ!!')
 st.text('あなたの現在地は？　※参考緯度経度　東京：36,140')
 la = st.text_input('緯度')
 lo = st.text_input('経度')
+if la and lo != '': 
+    d = [int(la)-52, int(lo)-188]
+else:
+    pass
 
 #視覚的要素だけの"Loading"機能実装
 if st.checkbox('記入OK!'):
@@ -25,7 +29,7 @@ if st.checkbox('記入OK!'):
 # 入力された緯度経度を元にデタラメな緯度経度データを作成する
     st.title('行先が決まりました！こちらがあなたのお勧めお散歩ポイントです。')
     osanpo_data = pd.DataFrame(
-        data=[[int(la)-52,int(lo)-188]],
+        data=[d],
         index=['目的地'],
         columns=['x','y']
     )
@@ -39,15 +43,18 @@ if st.checkbox('記入OK!'):
                 location=[r.x, r.y],
                 popup=index,
             ).add_to(m)
-
-    #スライダーをつける
-    rad = st.slider('目的地を中心とした円の半径 (km)',
-                    value=40,min_value=5, max_value=50)
-    # 半径の距離
-    # st.subheader('目的地からの距離{:,}km'),format(rad)
+            
+            # サークルマーカープロット(外径色は赤，内径色は青)
+            folium.CircleMarker(
+                location=d,
+                radius=40,
+                color='#ff0000',
+                fill_color='#0000ff'
+            ).add_to(m)
+            
     st.subheader(f'あなたにピッタリの目的地ですね')
     # 地図の初期設定
-    m = folium.Map(location=[int(la)-52,int(lo)-188], zoom_start=7)
+    m = folium.Map(location=d, zoom_start=7)
     # データを地図に渡す
     Destination(osanpo_data,m)
     #地図情報表示
